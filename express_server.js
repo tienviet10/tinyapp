449cb918785ce08e536fb1ee33ebcdf1e664dc48
Viet Tran <viettran101294@gmail.com>
2023-01-23 15:48:48 -0500

    add post method and the post url endpoint to the app

--- express_server.js
+++ express_server.js
@@ -3,29 +3,51 @@ const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = () => {
+  let res = "";
+  for (let i = 0; i < 6; i++) {
+    res += (String.fromCharCode(97 + Math.floor(Math.random() * 26)));
+  }
+  return res;
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
+
+
 
 app.get("/hello", (req, res) => {
   // res.send("<html><body>Hello <b>World</b></body></html>\n");
