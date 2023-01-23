f54b3a72830ccf428ddd591103c7e70e73188e11
Viet Tran <viettran101294@gmail.com>
2023-01-23 15:05:47 -0500

    add url show ejs

--- express_server.js
+++ express_server.js
@@ -22,6 +22,11 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   // res.send("<html><body>Hello <b>World</b></body></html>\n");
   const templateVars = { greeting: "Hello World!" };
