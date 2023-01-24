4e57feee34f1b83f631a176b82c6e56b2608f677
Viet Tran <viettran101294@gmail.com>
2023-01-24 00:09:46 -0500

    add logout functionality

--- express_server.js
+++ express_server.js
@@ -35,6 +35,12 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/logout", (req, res) => {
+  username = "";
+  res.clearCookie('username');
+  res.redirect("/urls");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
