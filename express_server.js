a6edd6c1ab4275af1051c10276d5ae2cef8295df
Viet Tran <viettran101294@gmail.com>
2023-01-23 20:34:17 -0500

    add edit long url functionality

--- express_server.js
+++ express_server.js
@@ -43,6 +43,20 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  if (req.params.id in urlDatabase) {
+    delete urlDatabase[req.params.id];
+  }
+  res.redirect("/urls");
+});
+
+app.post("/urls/:id/edit", (req, res) => {
+  if (req.params.id in urlDatabase) {
+    urlDatabase[req.params.id] = req.body.longURL;
+  }
+  res.redirect("/urls");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
@@ -52,6 +66,9 @@ app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id] ? urlDatabase[req.params.id] : "/";
   res.redirect(longURL);
 });
+
+
+
 
 app.get("/hello", (req, res) => {
   // res.send("<html><body>Hello <b>World</b></body></html>\n");
